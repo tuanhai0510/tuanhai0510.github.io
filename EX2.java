efb342f8b3e9e378caa35b1b0e7bcc2d95c4b7fb
tuanhai0510 <tuanhai.humg@gmail.com>
2021-03-30 16:18:50 +0700

    nopbaitapb2

---
+++ EX2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+
+public class EX2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        float x1, x2, delta;
+        a = sc.nextInt();
+        System.out.println("a = " + a);
+        b = sc.nextInt();
+        System.out.println("b = " + b);
+        c = sc.nextInt();
+        System.out.println("c = " + c);
+        delta = (b*b - (4*a*c))/(2*a);
+        if(delta<0) {
+            System.out.println("PT vo nghiem");
+        }else if(delta==0) {
+            x1=x2= (-b/(2*a));
+            System.out.println("PT co nghiem x1 = x2= " + x1);
+        }
+        else {
+            x1= (float) ((-b + sqrt(delta))/(2*a));
+            x2= (float) ((-b - sqrt(delta))/(2*a));
+            System.out.println("PT co 2 nghiem: x1 = " + x1 + "x2 = " + x2);
+        }
+    }
+}
+
+
